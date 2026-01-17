6f434580b0ac2467528a7e10f35ad8cc5dc0b8a0
CupSlash <briggs.hardy@ucas-edu.net>
2026-01-16 18:44:56 -0700

    PLP

--- individual_projects/personal_library_program.py
+++ individual_projects/personal_library_program.py
@@ -1,49 +1,71 @@
 #BH 2nd personal library program
 #Define the library
-library = set()
+library = []
+#Function to view the library
+def view_library(library):
+    if len(library) == 0:
+        print("Your library is empty.\n")
+        return
+    print("Here are the books in your library:\n")
+    for book in library:
+        print(f"{book[0]} by {book[1]}\n")
 #Function to add a new item
-def add_book():
-    new_book = input("What would you like to add to your library?\n")
-    library.add(new_book)
+def add_book(library):
+    new_title = input("What is the title of the book?\n")
+    new_author = input("Who is the author of the book?\n")
+    new_book = (new_title, new_author)
+    library.append(new_book)
 #Function to remove an item
-def remove_book():
-    print(library)
-    old_book = input("What book would you like to remove from your library?\n")
-    if old_book in library:
-        library.remove(old_book)
+def remove_book(library):
+    message = "Enter the number of the book you would like to remove.\n"
+    book_num = 1
+    for book in library:
+        message += f"{book_num}. {book[0]} by {book[1]}\n"
+        book_num += 1
+    selected_book_num = int(input(message))
+    if selected_book_num <= len(library):
+        del library[selected_book_num - 1]
     else:
         print("That book is not in your library")
 #Function to search the items
-def search_book():
-    book_to_search_for = input("What book would you like to search for?\n")
-    if book_to_search_for in library:
-        print("That book is in your library!")
-    else:
-        should_cpu_add_book = input("That book is not in your library, would you like me to add it? y/n\n")
-        if should_cpu_add_book == "y":
-            library.add(book_to_search_for)
-        else:
-            pass
+def search_book(library):
+    selected_option= input("What would you like to search by?\n 1. Title \n 2. Author \n")
+    book_found = False
+    if selected_option == "1":
+        title_to_search_for = input("What book title would you like to search for?\n")
+        for book in library:
+            if book[0] == title_to_search_for:
+                print(f"{book[0]} by {book[1]}\n")
+                book_found = True
+    elif selected_option == "2":
+        author_to_search_for = input("What author would you like to search for?\n")
+        for book in library:
+            if book[1] == author_to_search_for:
+                print(f"{book[0]} by {book[1]}\n")
+                book_found = True
+    if book_found == False:
+        print("No search results.")
 #function to run the game
-def start_library():
+def start_library(library):
     while True:
     #ask user what they would like to do
         choice = input("What would you like to do? \n1. View my library \n2. Add to my library \n3. Remove from my library \n4. Search for a book in my library \n5. Exit \n")
         if choice == "1":
-            print(library)
+    #Function to view the library
+            view_library(library)
         elif choice == "2":
     #Function to add a new item
-            add_book()
+            add_book(library)
         elif choice == "3":
     #Function to remove an item
-            remove_book()
+            remove_book(library)
         elif choice == "4":
     #Function to search the items
-            search_book()
-            exit
+            search_book(library)
         elif choice == "5":
             print("Bye-bye!")
             break
         else:
             print("That is not an option.")
-start_library()
+start_library(library)
+
