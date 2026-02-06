87cac81ee7e29fdeec7f2f92fd7f75a6f5a31379
CupSlash <briggs.hardy@ucas-edu.net>
2026-02-06 10:50:49 -0700

    bye

--- individual_projects/movie_recommender.py
+++ individual_projects/movie_recommender.py
@@ -1,17 +1,6 @@
 #BH 2nd Movie Recommender
-#functions for searching by a quality
-def search_by_title():
-    pass
-def search_by_director():
-    pass
-def search_by_genre():
-    pass
-def search_by_rating():
-    pass
-def search_by_length():
-    pass
-def search_by_actors():
-    pass
+#import csv
+import csv
 #main function that runs the code
 def main():
     #one-time introduction to the application
@@ -19,21 +8,10 @@ def main():
     #while loop so code repeats
     while True:
         #main menu
-        choice = ("Would you like to find a movie by title (t), director (d), genre (g), rating (r), length (l), notable actors (a), or exit (e)?")
-        if choice == "t":
-            search_by_title()
-        elif choice == "d":
-            search_by_director
-        elif choice == "g":
-            search_by_genre
-        elif choice == "r":
-            search_by_rating
-        elif choice == "l":
-            search_by_length
-        elif choice == "a":
-            search_by_actors
-        elif choice == "e":
-            print("See you later!")
+        continue_or_exit = input("Would you like to find a movie (m) or exit (e)?")
+        if continue_or_exit == "m":
+            
+        elif continue_or_exit == "e":
+            break
         else:
             print("That is not an option.")
-main()
