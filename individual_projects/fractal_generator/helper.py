865d95d38a831dacaf75aa367a603eb61fd37b8e
CupSlash <briggs.hardy@ucas-edu.net>
2026-02-27 18:51:36 -0700

    TRAINGE Ayoyu SEE

--- individual_projects/fractal_generator/helper.py
+++ individual_projects/fractal_generator/helper.py
@@ -1,24 +1,32 @@
 #BH 2nd fractal_generator helper.py
 #imports
+import math
 from turtle import *
 #function for drawing a triangle in turtle graphics
-def draw_triangle():
-    turtle = Turtle
-    screensize(200,200)
-    fillcolor()
-    begin_fill()
-    speed(0)
-    tracer(0)
-    hideturtle()
+#set turtle mechanics
+def draw_triangle(bottom_left_coord, side_length):
     penup()
-    goto(200, 200)
+    goto(bottom_left_coord[0], bottom_left_coord[1])
     pendown()
-    forward(200)
-    left(60)
-    forward(200)
-    penup()
-    end_fill()
-    done()
+    forward(side_length)
+    left(120)
+    forward(side_length)
+    left(120)
+    forward(side_length)
+    left(120)
 
-def sierpinski():
-    pass
+def draw_recursive_triangles(bottom_left_coord, side_length, remaining_depth):
+    draw_triangle(bottom_left_coord, side_length)
+    if remaining_depth == 0:
+        return
+    new_remaining_depth = remaining_depth - 1
+    triangle_height = math.sqrt(side_length**2 - (side_length/2)**2)
+    draw_recursive_triangles(bottom_left_coord, side_length/2, new_remaining_depth)
+    draw_recursive_triangles((bottom_left_coord[0] + side_length/4, bottom_left_coord[1] + triangle_height/2), side_length/2, new_remaining_depth)
+    draw_recursive_triangles((bottom_left_coord[0] + side_length/2, bottom_left_coord[1]), side_length/2, new_remaining_depth)
+def render():
+    screensize(200,200)
+    bottom_left_coord = (-100, -100)
+    side_length = 200
+    draw_recursive_triangles(bottom_left_coord, side_length, 5)
+    done()
--- individual_projects/fractal_generator/main.py
+++ individual_projects/fractal_generator/main.py
@@ -6,20 +6,27 @@ def menu():
     #introduce the user
     print("Welcome to the Sierpinski Triangle Generator! This program creates a Sierpinski Triangle fractal using recursion.")
     #get valid user input for depth and color
-    while True:
-        depth = int(input("\nEnter recursion depth (1-10): "))
-        if depth not in range(1, 11):
-            print("Invalid input. Please enter a number between 1 and 10.")
-        else:
-            break
-    while True:
-        color = input("Enter triangle color, can be one of: red, blue, green, yellow, purple, orange, pink, brown, black, white: ")
-        if color not in ["red", "blue", "green", "yellow", "purple", "orange", "pink", "brown", "black", "white"]:
-            print("Invalid input. Please enter a valid color.")
-        else:
-            break
+    # while True:
+    #     depth = int(input("\nEnter recursion depth (1-10): "))
+    #     if depth not in range(1, 11):
+    #         print("Invalid input. Please enter a number between 1 and 10.")
+    #     else:
+    #         break
+    # while True:
+    #     color = input("Enter triangle color, can be one of: red, blue, green, yellow, purple, orange, pink, brown, black, white: ")
+    #     if color not in ["red", "blue", "green", "yellow", "purple", "orange", "pink", "brown", "black", "white"]:
+    #         print("Invalid input. Please enter a valid color.")
+    #     else:
+    #         break
+    is_instant = input("Would you like to watch the triangle being drawn? (y/n): ")
+    if is_instant == 'n':
+        speed(0)
+        tracer(0)
+        hideturtle()
+    else:
+        pass
     #tell user what they entered
-    print("Generating Sierpinski Triangle with depth", depth, "and color", color)
+    # print("Generating Sierpinski Triangle with depth", depth, "and color", color)
     #draw the triangle
-    draw_triangle()
+    render()
 menu()
