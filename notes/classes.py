6bca0cd7d1fe9bdab7a256cc0832275dd3fc1dd6
CupSlash <briggs.hardy@ucas-edu.net>
2026-03-17 10:27:50 -0600

    commit

---
+++ notes/classes.py
@@ -0,0 +1,25 @@
+#BH 2nd classes.py
+#example 1
+class Dog:
+    def __init__(self, name, breed, age):
+        self.name = name.capitalize()
+        self.breed = breed.title()
+        self.age = age
+    def __str__(self):
+        return f"Name: {self.name}\nBreed: {self.breed}\nAge: {self.age}"
+    def speak(self):
+        return "Bark"
+doug = Dog("Doug", "Golden Retriever", 3)
+print(doug.speak())
+#example 2
+class ClassSubject:
+    def __init__(self, name, room = None, teacher = "Ms. LaRose"):
+        self.name = name.title()
+        self.room = room
+        self.teacher = teacher
+    def __str__(self):
+        return f"Name: {self.name}\nRoom: {self.room}\nTeacher: {self.teacher}"
+first = ClassSubject("Computer Programming 2", 200)
+second = ClassSubject("Computer Programming 2", 200)
+third = ClassSubject("Computer Science Principles", 200)
+print(first, second, third)
