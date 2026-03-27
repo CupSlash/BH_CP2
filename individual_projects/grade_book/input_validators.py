a7fa5edd6c68220d89ed8d89d37d5c8955d2ca72
CupSlash <briggs.hardy@ucas-edu.net>
2026-03-27 15:18:56 -0600

    Rename input validation function
    Renamed function from validate_score_input to check_float_value.

--- individual_projects/grade_book/input_validators.py
+++ individual_projects/grade_book/input_validators.py
@@ -1,6 +1,6 @@
 #BH 2nd input_validators.py
 # Check to see if the user input is a positive number between 0 and 100. If it is, return the number. If it isn't, ask them to try again until they enter a valid number.
-def validate_score_input(value):
+def check_float_value(value):
     message = "Please enter a positive number between 0 and 100."
     while True:
         try:
